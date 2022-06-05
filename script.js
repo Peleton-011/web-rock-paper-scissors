66c21346ae28a6abea9fdb8f5a77b538e2e7963f
Nico <nicodiazan@gmail.com>
2022-06-06 00:57:13 +0200

    Add computerPlay() and reformat the file

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 
-//Gets inputs, returns 1 if there was an error, 2 for rock, 3 for paper and 4 for scissors
+//Gets inputs, returns 3 if there was an error, 0 for rock, 1 for paper and 2 for scissors
 
-function getInput () {
+function userPlay () {
     let input = String(prompt("Choose Rock (r), Paper (p) or Scissors(s)"))
 
+    //Any input is valid so long as it begins with r, p or s case insensitive
     switch (input[0]){
         case "r":
         case "R":
-            return 2;
+            return 0;
             break;
         case "p":
         case "P":
-            return 3;
+            return 1;
             break;
         case "s":
         case "S":
-            return 4;
+            return 2;
             break;
         default:
             console.log("Sorry, there was an error")
-            return 1;
+            return 3;
     }
+}
+
+function computerPlay () {
+    //Random number from 0 to 1, cut to 3 decimal places, times 1000, modulo 3, so we get [0,2]
+    let choice = (Math.random().toFixed(3) * 1000) % 3;
+    return choice;
 }
